b416859d0d59473f181a52b5c6ee1d64e707729a
Andrew <andrewjlim@icloud.com>
2019-06-09 19:11:00 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var myInit = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        mode: 'cors',
+        cache: 'default'
+    };
+
+let myRequest = new Request("./data_class.json", myInit);
+
+fetch(myRequest)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        console.log(data.students);
+    })
